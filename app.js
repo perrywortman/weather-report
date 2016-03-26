c6df1af6fd254918dd19056ee41ab43a21102837
W. Perry Wortman <perrywortman@gmail.com>
2016-03-26 10:58:55 -0400

    updates printMessage function, package.json, readme, removes node_modules from repo

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var geocoder = require('geocoder');
 var url = 'https://api.forecast.io/forecast/';
 var apiKey = '3c255eba41175b4bc837ef23908d2efd' + '/';
 
-// Todo: add ability to pass in a zipcode as a node argv
-var zipcode = process.argv[2];
+var location = process.argv[2];
 
-geocoder.geocode(zipcode, function(err, data){
+geocoder.geocode(location, function(err, data){
   var body = ''
   if (!err) {
     var lat = data.results[0].geometry.location.lat;
@@ -16,7 +15,7 @@ geocoder.geocode(zipcode, function(err, data){
     url += apiKey;
     url += coordinates;
     https.get(url, function(response){
-      response.setEncoding('utf-8')
+      //response.setEncoding('utf-8')
       if(response.statusCode === 200){
         response.on('data', (chunk) => {
             body += chunk;
@@ -35,5 +34,5 @@ geocoder.geocode(zipcode, function(err, data){
 });
 
 function printMessage(conditions, temperature){
-  console.log('The current weather conditions in Central Park are ' + conditions.toLowerCase() + ', with a temperature of ' + temperature + '°F');
+  console.log('The current weather conditions for ' + location + ' are ' + conditions.toLowerCase() + ', with a temperature of ' + temperature + '°F');
 };
--- readme.md
+++ readme.md
@@ -1,7 +1,9 @@
 # Command Line Weather App
-Uses [node.js](https://nodejs.org/en/docs/) to pull data from the [forecast.io](https://developer.forecast.io/) weather API to display the current conditions and temperature for Central Park in New York.
+Uses [node.js](https://nodejs.org/en/docs/) to pull data from the [forecast.io](https://developer.forecast.io/) weather API to display the current conditions and temperature for the argument that you pass to node. Can be a zipcode or a city. Note that the city must be passed as a string, ex. `"Paris, TX"` unless you can pass as a single word `Paris`(will default to Paris, FR).
 
-Run from terminal: `node app.js`  
+Run from terminal: `node app.js 90210` or `node app.js "Monkey's Eyebrow, AZ"`  
 
 ## Todo
-Add ability to pass a zipcode as an argv using the npm module [geocoder](https://www.npmjs.com/package/geocoder).
+* ~~Add ability to pass a zipcode as an argv using the npm module [geocoder](https://www.npmjs.com/package/geocoder).~~
+* Add a client-side interface for mobile and desktop
+* Play a Weather Report snippet depending on the current conditions for improved accessibility
