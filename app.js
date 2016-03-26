ff26ff186482f04a0b6167e5b82815a4d7d38695
Ben Wilhelm <bencwilhelm@gmail.com>
2016-03-26 00:44:08 -0400

    puts api call into geocode callback

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 var https = require('https');
-// var geocoder = require('geocoder');
+var geocoder = require('geocoder');
 
 var url = 'https://api.forecast.io/forecast/';
 var apiKey = '3c255eba41175b4bc837ef23908d2efd' + '/';
-var location = '40.7833,-73.9667';
 
 // Todo: add ability to pass in a zipcode as a node argv
-// var zipcode = process.argv[2];
-// var location = geocoder.geocode(zipcode, function(error, data){
-//   if(error === null){
-//     var lat = data.results[0].geometry.location.lat;
-//     var lng = data.results[0].geometry.location.lng;
-//     var coordinates = lat + ',' + lng;
-//     url += coordinates;
-//   } else {
-//     console.error(error);
-//   }
-// });
+var zipcode = process.argv[2];
 
-var request = https.get(url + apiKey + location, function(response){
-  if(response.statusCode === 200){
-    var body = '';
-    response.on('data', (chunk) => body += chunk);
-    response.on('end', function(){
-      var parsedBody = JSON.parse(body);
-      var currentConditions = parsedBody.currently.summary;
-      var currentTemperature = Math.round(parsedBody.currently.temperature);
-      printMessage(currentConditions, currentTemperature);
+geocoder.geocode(zipcode, function(err, data){
+  var body = ''
+  if (!err) {
+    var lat = data.results[0].geometry.location.lat;
+    var lng = data.results[0].geometry.location.lng;
+    var coordinates = lat + ',' + lng;
+    url += apiKey;
+    url += coordinates;
+    https.get(url, function(response){
+      response.setEncoding('utf-8')
+      if(response.statusCode === 200){
+        response.on('data', (chunk) => {
+            body += chunk;
+        });
+        response.on('end', function(){
+          var parsedBody = JSON.parse(body);
+          var currentConditions = parsedBody.currently.summary;
+          var currentTemperature = Math.round(parsedBody.currently.temperature);
+          printMessage(currentConditions, currentTemperature);
+        });
+      }
     });
+  } else {
+    console.log(err);
   }
 });
 
