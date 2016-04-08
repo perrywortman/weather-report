8b53cf6dd5261261750959cf760e53c39dfb73dd
W. Perry Wortman <perrywortman@gmail.com>
2016-04-08 00:00:19 -0400

    remove semicolons, hide api key

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-var https = require('https');
-var geocoder = require('geocoder');
+var https = require('https')
+var geocoder = require('geocoder')
+var secretKey = require('./myKey')
 
-var url = 'https://api.forecast.io/forecast/';
-var apiKey = '3c255eba41175b4bc837ef23908d2efd' + '/';
+var url = 'https://api.forecast.io/forecast/'
+var apiKey = secretKey + '/'
 
-var location = process.argv[2];
+var location = process.argv[2]
 
 geocoder.geocode(location, function(err, data){
   var body = ''
   if (!err) {
-    var lat = data.results[0].geometry.location.lat;
-    var lng = data.results[0].geometry.location.lng;
-    var coordinates = lat + ',' + lng;
-    url += apiKey;
-    url += coordinates;
+    var lat = data.results[0].geometry.location.lat
+    var lng = data.results[0].geometry.location.lng
+    var coordinates = lat + ',' + lng
+    url += apiKey
+    url += coordinates
     https.get(url, function(response){
-      //response.setEncoding('utf-8')
       if(response.statusCode === 200){
         response.on('data', (chunk) => {
-            body += chunk;
-        });
-        response.on('end', function(){
-          var parsedBody = JSON.parse(body);
-          var currentConditions = parsedBody.currently.summary;
-          var currentTemperature = Math.round(parsedBody.currently.temperature);
-          printMessage(currentConditions, currentTemperature);
-        });
+            body += chunk
+        })
+        .on('end', function(){
+          var parsedBody = JSON.parse(body)
+          var currentConditions = parsedBody.currently.summary
+          var currentTemperature = Math.round(parsedBody.currently.temperature)
+          printMessage(currentConditions, currentTemperature)
+        })
       }
-    });
+    })
   } else {
-    console.log(err);
+    console.log(err)
   }
-});
+})
 
 function printMessage(conditions, temperature){
-  console.log('The current weather conditions for ' + location + ' are ' + conditions.toLowerCase() + ', with a temperature of ' + temperature + '°F');
-};
+  console.log('The current weather conditions for ' + location + ' are ' + conditions.toLowerCase() + ', with a temperature of ' + temperature + '°F')
+}
